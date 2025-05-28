27d91519cf9bc916b20dd9043169bd6a22d3b67a
pocopota <pocopota.program@gmail.com>
2025-05-28 15:37:50 +0900

    ログイン/ログイン状態保持実装

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,24 @@
-import { Button } from "antd";
+"use client"
+
 import styles from "./Header.module.scss";
+import LoginButton from "./LoginButton";
+
+import { useAuth } from "../context/AuthContext";
 
 export default function Header() {
+  const {user, loading} = useAuth();
+
   return (
     <header className={styles.header}>
       <div className={styles.headerInner}>
         <div className={styles.logo}>YadoPOS</div>
-        <div>
-          <Button>Googleでログイン</Button>
+        <div className={styles.account}>
+          {!loading && !user &&(
+            <LoginButton/>
+          )}
+          {!loading && user &&(
+            <div>ログイン済み</div>
+          )}
         </div>
       </div>
     </header>
--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // 必要に応じて
 import { getStorage } from "firebase/storage"; // 必要に応じて
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,4 +21,4 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const provider = new GoogleAuthProvider();
